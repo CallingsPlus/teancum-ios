f0e7f437a2e53c0ca63f120f2003a3f95cd975ea
Porter Hoskins <porterhoskins@gmail.com>
2023-09-10 22:44:53 -0600

    Invite and Join a Unit

--- Common/Sources/FirebaseClient/FirebaseClient.swift
+++ Common/Sources/FirebaseClient/FirebaseClient.swift
@@ -24,19 +24,53 @@ public enum FirebaseClient {
         LogEvent(.debug, "\(Self.self) configured").log()
     }
     
-    /// Returns: ID of the Unit
+    /// - Returns: ID of the Unit
     public static func createUnit(name: String) async throws -> String {
         let request = [
-            "name", name
+            "name": name
         ]
         
         let response = try await functions.httpsCallable("units-create").call(request).data as? [String: Any] ?? [:]
         
-        // TODO: Throw here if the ID doesn't exist 
-        return response["id"] as! String
+        return try await forceClaimRefreshForUnitChange()
     }
     
     public static func getUnit(id: String) async throws -> Unit {
         return try await firestore.collection("units").document(id).getDocument(as: Unit.self)
+    }
+    
+    public static func getUnitInviteToken() async throws -> String {
+        let response = try await functions.httpsCallable("units-invite").call().data as? [String: Any] ?? [:]
+        
+        // TODO: Throw here if the token doesn't exist
+        return response["token"] as! String
+    }
+    
+    /// - Returns: ID of the Unit
+    public static func joinUnit(with inviteToken: String) async throws -> String {
+        let request = [
+            "token": inviteToken
+        ]
+        
+        let response = try await functions.httpsCallable("units-join").call(request).data as? [String: Any] ?? [:]
+        
+        return try await forceClaimRefreshForUnitChange()
+    }
+    
+    /// Force the token to refresh with the new claim set from the server
+    /// - Returns: ID of the Unit
+    private static func forceClaimRefreshForUnitChange() async throws -> String {
+        var unit: String?
+        while true {
+            print("Trying to refresh token for unit change...")
+            let user = try await authentication.currentUser?.getIDTokenResult(forcingRefresh: true)
+            if let unitClaim = user?.claims["unit"] as? String {
+                unit = unitClaim
+                break
+            }
+        }
+        
+        // TODO: Throw here if the id doesn't exist
+        return unit!
     }
 }
