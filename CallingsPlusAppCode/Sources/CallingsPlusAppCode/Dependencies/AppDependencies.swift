67226d1a4ff146629337187940c1d2aa11635a19
Albert Bori <github@albertbori.com>
2023-09-30 11:04:01 -0400

    Fixed gitignore issue

---
+++ CallingsPlusAppCode/Sources/CallingsPlusAppCode/Dependencies/AppDependencies.swift
@@ -0,0 +1,8 @@
+import FirebaseClient
+import Foundation
+
+/// All dependency typealiases roll up to this class to be satisfied with concrete implementations
+/// For more information, ask Dude about Composed Protocol Dependency injection
+class AppDependencies: CallingsPlusAppDependencies {
+    // Global dependencies for standing up production configurations will go in here
+}
